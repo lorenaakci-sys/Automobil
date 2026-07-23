47bdf82f1798c5f8df53a891ee09c6c977b7046b
Lorena <lorena.akci@gmail.com>
2026-07-23 19:18:32 +0200

    get/set

--- src/Automobil.java
+++ src/Automobil.java
@@ -15,4 +15,38 @@ public class Automobil {
         this.trenutnaBrzina = 0;
         this.upaljeno = false;
     }
+
+    public String getMarka() {
+        return this.marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getGodinaProizvodnje() {
+        return this.godinaProizvodnje;
+    }
+
+    public void setGodinaProizvodnje(int godinaProizvodnje) {
+        this.godinaProizvodnje = godinaProizvodnje;
+    }
+
+    public String getRegistracija() {
+        return this.registracija;
+    }
+
+    public void setRegistracija(String registracija) {
+        this.registracija = registracija;
+    }
+
+
 }
